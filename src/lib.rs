f3be9cb84015542a3a505c72f36dec74a8ff39fb
Raphael Larsen <raphael.j.larsen@gmail.com>
2026-05-27 12:53:20 -0400

    Redesign around swapping Vec and adding PopN iterator.

--- src/lib.rs
+++ src/lib.rs
@@ -1,219 +1,282 @@
 #![no_std]
 use core::{
     cell::UnsafeCell,
-    mem::MaybeUninit,
+    mem::ManuallyDrop,
+    ops::Range,
     sync::atomic::{AtomicI32, AtomicU32, Ordering},
 };
 
 extern crate alloc;
-
-use alloc::{boxed::Box, sync::Arc, vec::Vec};
+use alloc::{sync::Arc, vec::Vec};
 
 use crossbeam_utils::CachePadded;
 
-/// A buffer allocation of T
-struct Publication<T> {
-    data: Box<[MaybeUninit<T>]>,
-}
+/// This is NOT a Vec<T>, but it's represnted as one for conveinence.
+///
+/// The `ptr` and `capacity` of the `Vec` are logically correct HOWEVER
+/// the `len` field is not, instead the len field represents the length of
+/// elements that will NOT be popped from the publication. Hence once
+/// all elements have been popped, the `len` field will yet again represent
+/// a valid `Vec` length.
+#[repr(transparent)]
+struct Publication<T>(ManuallyDrop<Vec<T>>);
 
 impl<T> Default for Publication<T> {
     fn default() -> Self {
-        Self { data: Box::new([]) }
+        Self(Default::default())
     }
 }
 
 impl<T> Publication<T> {
-    fn capacity(&self) -> usize {
-        self.data.len()
+    unsafe fn read(&self, index: usize) -> T {
+        unsafe { self.0.as_ptr().add(index).read() }
     }
 
-    /// Writes data to the publication. This will grow (amortized) the publication to fit the data.
-    ///
-    /// # Safety
-    /// - caller must ensure that `len == 0`
-    unsafe fn publish_assume_empty(&mut self, data: impl ExactSizeIterator<Item = T>) {
-        let trgt_cap = data.len();
-        let curr_cap = self.capacity();
-        if trgt_cap > curr_cap {
-            let amortized_cap = curr_cap * 2;
-            let new_cap = amortized_cap.max(trgt_cap);
-
-            self.data = (0..new_cap).map(|_| MaybeUninit::uninit()).collect();
-        }
-
-        for (index, val) in data.enumerate() {
-            // Safety: We just ensured that cap > len
-            unsafe {
-                self.data.get_unchecked_mut(index).write(val);
-            }
-        }
-    }
-
-    /// # Safety
-    /// - caller must ensure that `index < len` (and by extension, `index < self.capacity()`)
-    unsafe fn get_unchecked(&self, index: usize) -> T {
-        // Safety: Ensured by caller
-        unsafe { self.data.get_unchecked(index).as_ptr().read() }
+    fn pop_offset(&self) -> usize {
+        self.0.len()
     }
 }
 
-pub struct RemoteFreeFreeList<T> {
+struct SharedState<T> {
     publication: UnsafeCell<Publication<T>>,
-    claim: CachePadded<AtomicI32>,
-    len: CachePadded<AtomicU32>,
+    head: CachePadded<AtomicI32>,
+    tail: CachePadded<AtomicU32>,
 }
 
-unsafe impl<T: Send> Sync for RemoteFreeFreeList<T> {}
+/// Safety: The structure ensures that mutable, and immutable access of publication
+/// don't happen at the same time.
+unsafe impl<T: Send> Sync for SharedState<T> {}
 
-impl<T> Default for RemoteFreeFreeList<T> {
+impl<T> Default for SharedState<T> {
     fn default() -> Self {
         Self {
-            publication: UnsafeCell::new(Publication::default()),
-            claim: CachePadded::new(AtomicI32::new(0)),
-            len: CachePadded::new(AtomicU32::new(0)),
+            publication: Default::default(),
+            head: Default::default(),
+            tail: Default::default(),
         }
     }
 }
 
-impl<T> RemoteFreeFreeList<T> {
-    /// Returns weather or not all values of the Publication have been poped.
-    pub fn is_empty(&self) -> bool {
-        self.len.load(Ordering::Acquire) == 0
+impl<T> SharedState<T> {
+    /// # Safety
+    /// - You must be the exclusive caller of this function.
+    unsafe fn try_publish(&self, data: &mut Vec<T>) {
+        // `Ordering::Acquire` ensures any reads to `publication` happen before any prospective writes.
+        if self.tail.load(Ordering::Acquire) == 0 {
+            // Safety: `SharedState::pop` only reads publication if it knows `tail != 0`.
+            let publication = unsafe { self.publication.get().as_mut_unchecked() };
+
+            let len = data.len();
+            // This ratio is chosen arbitrarily.
+            let pop_to = len / 2;
+            // Safety: This is a little weird. In a publication the `len` field is actually
+            // the final index we're allowed to pop. The _actual_ length is a combination of `self.tail`
+            // and `self.head`. Once all (allowed) items have been popped the length of the vector will be
+            // equal to the `pop_to` value: hence why we are safely useing the old publications vector as
+            // our new local vector.
+            // The reason we're doing this is because we want to share items between the local `Vec` and
+            // the `Publication`, if we moved ALL the items then any future calls to pop from the local
+            // `Vec` would fail (the local vec is our fast path).
+            unsafe {
+                data.set_len(pop_to);
+            }
+
+            core::mem::swap(data, &mut publication.0);
+
+            // the number of elements that will be popped.
+            let eff_len = (len - pop_to) as u32;
+            // `Ordering::Relaxed` because `head` fences the publication and we don't need
+            // to fence with the `tail.load` because this design only allows for a single producer.
+            // This store needs to happen before head as readers always expect tail to be >= head.
+            self.tail.store(eff_len, Ordering::Relaxed);
+            // `Ordering::Release` ensures that readers can see the published data.
+            self.head.store(eff_len as i32, Ordering::Release);
+        }
     }
 
-    /// Returns the next value from the list. Keep in mind that this is a best effort pop and
-    /// may return `None` even if there are recyclable values.
-    ///
-    /// If you call this function a large number of times (`i32::MAX - 1`) before a single `sync`
-    /// call in the owning `FreeFreeList` the behavior is undefined.
-    pub fn pop(&self) -> Option<T> {
-        let index = self.claim.fetch_sub(1, Ordering::Acquire).wrapping_sub(1);
+    fn pop(&self) -> Option<T> {
+        // `Ordering::Acquire` ensures we see any writes to `publication`.
+        let index = self.head.fetch_sub(1, Ordering::Acquire).wrapping_sub(1);
         if index < 0 {
-            // This should basically never happen BUT is completely dependent on the usage patterns of the caller.
-            debug_assert_ne!(
-                index,
-                i32::MIN,
-                "`claim` overflow; more than {} pops have occurred since the last len check",
-                i32::MIN.abs()
-            );
+            debug_assert_ne!(index, i32::MIN);
             return None;
         }
 
-        // Safety: The publication is only modified when all items have been (completely) popped and we are
-        // in the middle of popping a value. And the index is obtained from a decriment only value that
-        // was initalized to the `len` and so is ensured to be in bounds.
-        let value = unsafe {
-            self.publication
-                .get()
-                .as_ref_unchecked()
-                .get_unchecked(index as usize)
-        };
-
-        // Cannot overflow because len is always >= claim and we don't decrement unless claim >= 0.
-        // We must decriment this separately than `claim` because we can only write a new
-        // publication when all items have been claimed AND read.
-        self.len.fetch_sub(1, Ordering::Release);
-
+        // We must only fetch publication after we've checked that `head <= 0` because
+        // the producer is allowed to publish if the tail is zero (and all we know is that the
+        // tail is >= head).
+        let publication = unsafe { self.publication.get().as_ref_unchecked() };
+        let index = index as usize + publication.pop_offset();
+        // Safety: `index` is guaranteed to be in bounds b/c `head` starts at the initalized length
+        // and is decriment only and is checked against 0.
+        let value = unsafe { publication.read(index) };
+        // `Ordering::Release` ensures that this read happens before the decrement of `tail`.
+        self.tail.fetch_sub(1, Ordering::Release);
         Some(value)
     }
 
-    /// # Safety
-    /// - caller must ensure that `len == 0`
-    /// - must not be called concurrently with itself (exclusive publisher)
-    unsafe fn publish_assume_exclusive_empty(&self, data: impl ExactSizeIterator<Item = T>) {
-        let len = data.len();
-        debug_assert!(len < i32::MAX as usize);
-        // Safety: ensured by caller
-        unsafe {
-            self.publication
-                .get()
-                .as_mut_unchecked()
-                .publish_assume_empty(data);
+    fn pop_n(&self, n: u32) -> PopN<'_, T> {
+        // `Ordering::Acquire` ensures we see any writes to `publication`.
+        let range_to = self.head.fetch_sub(n as i32, Ordering::Acquire);
+        if range_to <= 0 {
+            debug_assert!(range_to.wrapping_sub_unsigned(n) < 0);
+            return PopN(None);
         }
-        // len must be updated first to ensure that poppers don't see a stale `len` value but a
-        // up-to-date `claim` value.
-        // Is `Ordering::Relaxed` because `claim` is the atomic boundary for pop operations. We don't
-        // need a memory fence for other `len` checks because this is an exclusive publisher.
-        self.len.store(len as u32, Ordering::Relaxed);
-        // `Ordering::Release` to ensure that the `publication` is visible to other threads.
-        self.claim.store(len as i32, Ordering::Release);
+        let range_to = range_to as u32;
+        let range_from = range_to.saturating_sub(n);
+        let n = range_from - range_to;
+
+        // Safety: Since `tail >= head > 0` we know nobody is concurrently modifying the publication.
+        let publication = unsafe { self.publication.get().as_ref_unchecked() };
+
+        let range = (range_from as usize + publication.pop_offset())
+            ..(range_to as usize + publication.pop_offset());
+        // Safety: The `range` is guaranteed to be in bounds of the publication as it is bounded between
+        // the initially set `head` value and initally set `pop_offset` value. And each index only occurs once.
+        PopN(Some(unsafe {
+            InnerPopN::new(publication, range, &self.tail, n)
+        }))
     }
 }
 
-impl<T> Drop for RemoteFreeFreeList<T> {
+impl<T> Drop for SharedState<T> {
     fn drop(&mut self) {
-        let len = *self.len.get_mut();
         let publication = self.publication.get_mut();
-        for index in 0..len as usize {
-            // Safety: len is the number of initalized items in the collection.
-            let val = unsafe { publication.get_unchecked(index) };
-            drop(val);
+        let len = *self.tail.get_mut() as usize + publication.pop_offset();
+
+        for index in 0..len {
+            // Safety: The number of elements in the publication is the addition
+            // of the pop_offset and the `tail` count, so `index` is guaranteed to be in bounds.
+            unsafe { drop(publication.read(index)) };
         }
     }
 }
 
-/// A `FreeFreeList` that can hand out a `RemoteFreeFreeList` to cheaply pop items from any thread.
-pub struct FreeFreeList<T> {
-    remote: Arc<RemoteFreeFreeList<T>>,
+pub struct PopN<'a, T>(Option<InnerPopN<'a, T>>);
+
+impl<'a, T> Iterator for PopN<'a, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.as_mut().and_then(InnerPopN::next)
+    }
+}
+
+struct InnerPopN<'a, T> {
+    publication: &'a Publication<T>,
+    range: Range<usize>,
+    tail: &'a AtomicU32,
+    n: u32,
+}
+
+impl<'a, T> InnerPopN<'a, T> {
+    // Safety: The `range` must be in bounds of the publication.
+    unsafe fn new(
+        publication: &'a Publication<T>,
+        range: Range<usize>,
+        tail: &'a AtomicU32,
+        n: u32,
+    ) -> Self {
+        Self {
+            publication,
+            range,
+            tail,
+            n,
+        }
+    }
+}
+
+impl<'a, T> Iterator for InnerPopN<'a, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.range.next().map(|index| {
+            // Safety: Ensured by caller of `PopN::new`.
+            unsafe { self.publication.read(index) }
+        })
+    }
+}
+
+impl<'a, T> Drop for InnerPopN<'a, T> {
+    fn drop(&mut self) {
+        for _ in self.into_iter() {}
+        // `Ordering::Release` ensures that all reads happen before the decrement of `tail`.
+        self.tail.fetch_sub(self.n, Ordering::Release);
+    }
+}
+
+/// A reader for a [`FreeList`] that provides methods for popping values.
+pub struct FreeListReader<T> {
+    shared: Arc<SharedState<T>>,
+}
+
+impl<T> FreeListReader<T> {
+    /// Pop a single value from the free list.
+    pub fn pop(&self) -> Option<T> {
+        self.shared.pop()
+    }
+
+    /// Pop up to `n` values from the free list.
+    pub fn pop_n(&self, n: u32) -> PopN<'_, T> {
+        self.shared.pop_n(n)
+    }
+}
+
+pub struct FreeList<T> {
+    shared: Arc<SharedState<T>>,
     local: Vec<T>,
 }
 
-impl<T> Default for FreeFreeList<T> {
+impl<T> core::ops::Deref for FreeList<T> {
+    type Target = Vec<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.local
+    }
+}
+
+impl<T> core::ops::DerefMut for FreeList<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.local
+    }
+}
+
+impl<T> Default for FreeList<T> {
     fn default() -> Self {
         Self {
-            remote: Arc::default(),
-            local: Vec::default(),
+            shared: Default::default(),
+            local: Default::default(),
         }
     }
 }
 
-impl<T> FreeFreeList<T> {
-    /// Creates a new `FreeFreeList`.
+impl<T> FreeList<T> {
+    /// Creates a new `FreeList`.
     pub fn new() -> Self {
         Self::default()
     }
 
-    /// Returns shared ownership to the `RemoteFreeFreeList`.
-    pub fn remote(&self) -> Arc<RemoteFreeFreeList<T>> {
-        self.remote.clone()
-    }
-
-    /// Returns a mutable reference to the `local` `Vec<T>`. This is where you can push (or pop) items.
-    ///
-    /// After pushing values to this `Vec` you should call [`FreeFreeList:;sync`] to synchronize with the `RemoteFreeFreeList`.
-    pub fn local_mut(&mut self) -> &mut Vec<T> {
-        &mut self.local
-    }
-
-    /// If the `RemoteFreeFreeList` is empty then we drain half of the `local` `Vec<T>` into it.
-    pub fn sync(&mut self) {
-        if self.remote.is_empty() {
-            let range_from = self.local.len() / 2;
-            let data = self.local.drain(range_from..);
-            // Safety: We just checked `is_empty()`.
-            // Since we have `&mut self`, we know no other thread will call
-            // `assume_exclusive_empty_publish` concurrently as this structure is
-            // the only way to (safely) publish to the `RemoteFreeFreeList`.
-            unsafe {
-                self.remote
-                    .publish_assume_exclusive_empty(data);
-            };
+    /// Returns a `FreeListReader` that can be used to pop from the `FreeList`.
+    pub fn reader(&self) -> FreeListReader<T> {
+        FreeListReader {
+            shared: self.shared.clone(),
         }
     }
 
-    /// Pops an item from the `local` `Vec<T>`. Keep in mind that this is a best-effort pop.
-    /// This may return `None` even if the `FreeFreeList` has items.
-    pub fn pop_local(&mut self) -> Option<T> {
-        self.local.pop()
+    /// Synchronizes the local `FreeList` with the shared state.
+    ///
+    /// This method must be called frequently. How frequently depends on your usage.
+    /// To be specific:
+    /// 1. The remote free list will never have any items.
+    /// 2. If this is not called between i32::MAX failed remote pop calls there will be undefined behavior.
+    pub fn sync(&mut self) {
+        // Safety:
+        unsafe {
+            self.shared.try_publish(&mut self.local);
+        }
     }
 
-    /// Pops an item from the `remote` [`RemoteFreeFreeList`]. Keep in mind that this is a best-effort pop.
-    /// This may return [`None`] even if the [`RemoteFreeFreeList`] has items.
-    pub fn pop_remote(&self) -> Option<T> {
-        self.remote.pop()
-    }
-}
-
-fn a() {
-    let a = Arc::get_mut(this)
+    // I do not provide methods for remote popping as you should always pop locally. If you need
+    // strong guarantees (that popping will always return a value if there is one) then you should
+    // not use this type.
 }
