c6eb3d4c6e6b1c800eb8d501cde3cbaa3600a518
Raphael Larsen <raphael.j.larsen@gmail.com>
2026-05-28 10:06:04 -0400

    Double-buffered & major refactor.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use core::{
     iter::FusedIterator,
     mem::{self, ManuallyDrop},
     ops::{Deref, DerefMut, Range},
-    sync::atomic::{AtomicI32, AtomicU32, Ordering},
+    sync::atomic::{AtomicBool, AtomicI32, AtomicU32, Ordering},
 };
 
 extern crate alloc;
@@ -12,13 +12,7 @@ use alloc::{sync::Arc, vec::Vec};
 
 use crossbeam_utils::CachePadded;
 
-/// This is NOT a Vec<T>, but it's represnted as one for conveinence.
-///
-/// The `ptr` and `capacity` of the `Vec` are logically correct HOWEVER
-/// the `len` field is not, instead the len field represents the length of
-/// elements that will NOT be popped from the publication. Hence once
-/// all elements have been popped, the `len` field will yet again represent
-/// a valid `Vec` length.
+/// A `Vec` that is in the process of being drained.
 #[repr(transparent)]
 #[derive(Debug)]
 struct Publication<T>(ManuallyDrop<Vec<T>>);
@@ -30,27 +24,54 @@ impl<T> Default for Publication<T> {
 }
 
 impl<T> Publication<T> {
+    /// Swaps the previous `Vec` as if it had been drained to the previous `drain_from` index
+    /// with the `other` `Vec` that will be drained to `drain_from`.
+    ///
+    /// # Safety
+    /// - must have been drained to the previous `drain_from` value
+    /// - `drain_from` must be less than or equal to the length of `other`
+    unsafe fn swap_as_drained(&mut self, other: &mut Vec<T>, drain_from: usize) {
+        // Safety: the next time `swap_as_drained` is called this length will be correct
+        unsafe {
+            other.set_len(drain_from);
+        }
+        mem::swap(&mut *self.0, other);
+    }
+
+    /// Returns the value at `index` in the `Vec`.
+    ///
+    /// # Safety
+    /// - `index` must be less than the length of the `Vec`
+    /// - the item at `index` must have been previously `initalized`
+    /// - the item at `index` must not have been previously `read`
     unsafe fn read(&self, index: usize) -> T {
         unsafe { self.0.as_ptr().add(index).read() }
     }
 
-    fn pop_offset(&self) -> usize {
+    /// Returns the final length of the `Vec` after draining.
+    fn drain_from(&self) -> usize {
         self.0.len()
     }
 }
 
+/// A wrapper around a `Vec` that facilitates mulitple threads popping
+/// values concurrently as well as swapping in a new `Vec` when the old
+/// one is drained.
 #[derive(Debug)]
-struct SharedState<T> {
+struct SharedPopVec<T> {
+    /// The `Vec` being drained. As long as `tail != 0` this is immutable. When `tail == 0` the
+    /// single producer has exclusive mutable access.
     publication: UnsafeCell<Publication<T>>,
+    /// The number of items that are initalized and not currently being drained.
+    /// Readers decriment this to reserve an item for reading.
     head: CachePadded<AtomicI32>,
+    /// The number of items remaining to be drained from `publication`.
+    /// The `tail` is always `>= head` because they are initalized to the same
+    /// value and the `tail` is decrimented after the `head` is decrimented.
     tail: CachePadded<AtomicU32>,
 }
 
-/// Safety: The structure ensures that mutable, and immutable access of publication
-/// don't happen at the same time.
-unsafe impl<T: Send> Sync for SharedState<T> {}
-
-impl<T> Default for SharedState<T> {
+impl<T> Default for SharedPopVec<T> {
     fn default() -> Self {
         Self {
             publication: Default::default(),
@@ -60,124 +81,135 @@ impl<T> Default for SharedState<T> {
     }
 }
 
-impl<T> SharedState<T> {
-    /// # Safety
-    /// - You must be the exclusive caller of this function.
-    unsafe fn try_publish(&self, data: &mut Vec<T>) {
-        // `Ordering::Acquire` ensures any reads to `publication` happen before any prospective writes.
-        if self.tail.load(Ordering::Acquire) == 0 {
-            // Safety: `SharedState::pop` only reads publication if it knows `tail != 0`.
-            let publication = unsafe { self.publication.get().as_mut_unchecked() };
-
-            let len = data.len();
-            // This ratio is chosen arbitrarily.
-            let pop_to = len / 2;
-            // Safety: This is a little weird. In a publication the `len` field is actually
-            // the final index we're allowed to pop. The _actual_ length is a combination of `self.tail`
-            // and `self.head`. Once all (allowed) items have been popped the length of the vector will be
-            // equal to the `pop_to` value: hence why we are safely useing the old publications vector as
-            // our new local vector.
-            // The reason we're doing this is because we want to share items between the local `Vec` and
-            // the `Publication`, if we moved ALL the items then any future calls to pop from the local
-            // `Vec` would fail (the local vec is our fast path).
-            unsafe {
-                data.set_len(pop_to);
-            }
-
-            mem::swap(data, &mut publication.0);
-
-            // the number of elements that will be popped.
-            let eff_len = (len - pop_to) as u32;
-            // `Ordering::Relaxed` because `head` fences the publication and we don't need
-            // to fence with the `tail.load` because this design only allows for a single producer.
-            // This store needs to happen before head as readers always expect tail to be >= head.
-            self.tail.store(eff_len, Ordering::Relaxed);
-            // `Ordering::Release` ensures that readers can see the published data.
-            self.head.store(eff_len as i32, Ordering::Release);
-        }
+impl<T> SharedPopVec<T> {
+    /// This just panics.
+    /// It is included to help with branch prediction, and put the panic message in one spot.
+    #[cold]
+    #[inline]
+    fn on_overflow() -> ! {
+        panic!("head overflow")
     }
 
-    fn pop(&self) -> Option<T> {
+    pub fn pop(&self) -> Option<T> {
         // `Ordering::Acquire` ensures we see any writes to `publication`.
         let index = self.head.fetch_sub(1, Ordering::Acquire).wrapping_sub(1);
         if index < 0 {
-            debug_assert_ne!(index, i32::MIN, "head overflow");
+            if index == i32::MAX {
+                Self::on_overflow();
+            }
             return None;
         }
 
-        // We must only fetch publication after we've checked that `head <= 0` because
-        // the producer is allowed to publish if the tail is zero (and all we know is that the
-        // tail is >= head).
+        // Safety: `publication` is under shared ownership because `tail >= head > 0`.
         let publication = unsafe { self.publication.get().as_ref_unchecked() };
-        let index = index as usize + publication.pop_offset();
-        // Safety: `index` is guaranteed to be in bounds b/c `head` starts at the initalized length
-        // and is decriment only and is checked against 0.
+        let index = index as usize + publication.drain_from();
+        // Safety: `index` is reserved from `head` and so is guaranteed to be in range and unread.
         let value = unsafe { publication.read(index) };
-        // `Ordering::Release` ensures that this read happens before the decrement of `tail`.
+        // `Ordering::Release` ensures that the `publication` read happens before the decrement of `tail`.
         self.tail.fetch_sub(1, Ordering::Release);
         Some(value)
     }
 
-    fn pop_n(&self, n: u32) -> PopN<'_, T> {
-        PopN::new(self, n)
+    fn pop_n(&self, n: u32) -> InnerPopN<'_, T> {
+        InnerPopN::new(self, n)
+    }
+
+    /// # Safety
+    /// - You must be the exclusive publisher to call this function.
+    /// - The `tail` MUST be equal to zero to avoid race conditions.
+    unsafe fn publish(&self, data: &mut Vec<T>) {
+        // Safety: When tail is zero the single publisher gets exclusive access to the `publication`.
+        let publication = unsafe { self.publication.get().as_mut_unchecked() };
+
+        let len = data.len();
+        let drain_from = data.len() / 2;
+
+        // Safety:
+        // - `drain_from` is guaranteed to be less or equal to than `data.len()`.
+        // - caller ensures that the publication was fully drained before swapping.
+        unsafe {
+            publication.swap_as_drained(data, drain_from);
+        }
+
+        let eff_len = (len - drain_from) as u32;
+        // `Ordering::Relaxed` because `head` fences the publication and we don't need
+        // to fence with `tail.load`s because this design only allows for a single producer.
+        // This store needs to happen before head as readers always expect tail to be >= head.
+        self.tail.store(eff_len, Ordering::Relaxed);
+        // `Ordering::Release` ensures that readers can see the published data.
+        self.head.store(eff_len as i32, Ordering::Release);
     }
 }
 
-impl<T> Drop for SharedState<T> {
+impl<T> Drop for SharedPopVec<T> {
     fn drop(&mut self) {
         let publication = self.publication.get_mut();
-        let len = *self.tail.get_mut() as usize + publication.pop_offset();
+        let tail = self.tail.get_mut();
+        let len = *tail as usize + publication.drain_from();
 
-        for index in 0..len {
-            // Safety: The number of elements in the publication is the addition
-            // of the pop_offset and the `tail` count, so `index` is guaranteed to be in bounds.
-            unsafe { drop(publication.read(index)) };
+        for i in 0..len {
+            // Safety: the length is the number of initalized and unpoped elements in the publication.
+            unsafe {
+                drop(publication.read(i));
+            }
         }
     }
 }
 
+/// Safety: The structure ensures that mutable, and immutable access of publication
+/// don't happen at the same time.
+unsafe impl<T: Send> Sync for SharedPopVec<T> {}
+
 #[derive(Debug)]
-pub struct PopN<'a, T> {
-    publication: &'a UnsafeCell<Publication<T>>,
-    tail: &'a AtomicU32,
+struct InnerPopN<'a, T> {
+    shared: &'a SharedPopVec<T>,
     range: Range<i32>,
-    poped: u32,
+    popped: u32,
 }
 
-impl<'a, T> PopN<'a, T> {
-    fn new(shared: &'a SharedState<T>, n: u32) -> Self {
+impl<'a, T> InnerPopN<'a, T> {
+    fn new(shared: &'a SharedPopVec<T>, n: u32) -> Self {
+        if n == 0 {
+            return Self {
+                shared,
+                range: 0..0,
+                popped: 0,
+            };
+        }
+
         // `Ordering::Acquire` ensures we see any writes to `publication`.
         let range_to = shared.head.fetch_sub(n as i32, Ordering::Acquire);
         let range_from = range_to.wrapping_sub_unsigned(n);
-        debug_assert!(range_from < range_to, "head overflow");
-        // When less than zero bring it back to zero... unless range_to is also less than zero,
-        // in which case they should be equal (so that poped is zero and range.next() returns None).
-        let range_from = range_from.max(0).min(range_to);
-        let poped = (range_to - range_from) as u32;
+        if range_from >= range_to {
+            SharedPopVec::<T>::on_overflow();
+        }
+        // This ensures that `range_from` is greater than `range_to` when `head` is less than zero,
+        // so that `range.next()` returns `None`.
+        let range_from = range_from.max(0);
+        // This ensures that when `range_from` is forced to be greater than `range_to` (by `max(0)`),
+        // `popped` is still zero.
+        let popped = (range_to.wrapping_sub(range_from)).max(0) as u32;
         let range = range_from..range_to;
 
         Self {
-            publication: &shared.publication,
-            tail: &shared.tail,
+            shared,
             range,
-            poped,
+            popped,
         }
     }
 }
 
-impl<'a, T> Iterator for PopN<'a, T> {
+impl<'a, T> Iterator for InnerPopN<'a, T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.range.next().map(|index| {
-            // Safety: During creation of `PopN` our `range` only has values if the `head > 0`.
-            // We do this now and not in `PopN::new` because in `PopN::new` we don't want to
-            // branch on `poped > 0`.
-            let publication = unsafe { self.publication.get().as_ref_unchecked() };
-            // usize cast doesn't wrap because if `range_to` is less than zero then `range_from == range_to` and no items will be yielded.
-            let index = index as usize + publication.pop_offset();
-            // Safety: `index` is within range which is bounded to `head + pop_offset` and `pop_offset` and no index is
-            // poped twice.
+            // Safety: During the creation of `PopN` our `range` only has values if `head > 0`.
+            // publication is shared immutable when `tail > 0` and `tail >= head.
+            let publication = unsafe { self.shared.publication.get().as_ref_unchecked() };
+            // `index` doesn't wrap because `range_from` is bound to `0`.
+            let index = index as usize + publication.drain_from();
+            // Safety: the `range` is reserved from `head` and bound by `0`.
             unsafe { publication.read(index) }
         })
     }
@@ -187,17 +219,119 @@ impl<'a, T> Iterator for PopN<'a, T> {
     }
 }
 
-impl<'a, T> ExactSizeIterator for PopN<'a, T> {}
+impl<'a, T> ExactSizeIterator for InnerPopN<'a, T> {}
+impl<'a, T> FusedIterator for InnerPopN<'a, T> {}
 
-impl<'a, T> FusedIterator for PopN<'a, T> {}
-
-impl<'a, T> Drop for PopN<'a, T> {
+impl<'a, T> Drop for InnerPopN<'a, T> {
     fn drop(&mut self) {
         for _ in self.into_iter() {}
         // `Ordering::Release` ensures that all reads happen before the decrement of `tail`.
-        self.tail.fetch_sub(self.poped, Ordering::Release);
+        if self.popped != 0 {
+            self.shared.tail.fetch_sub(self.popped, Ordering::Release);
+        }
     }
 }
+
+#[derive(Debug)]
+struct SharedState<T> {
+    a: SharedPopVec<T>,
+    b: SharedPopVec<T>,
+    prioritize_b: CachePadded<AtomicBool>,
+}
+
+impl<T> Default for SharedState<T> {
+    fn default() -> Self {
+        Self {
+            a: Default::default(),
+            b: Default::default(),
+            prioritize_b: Default::default(),
+        }
+    }
+}
+
+impl<T> SharedState<T> {
+    fn pop(&self) -> Option<T> {
+        if self.prioritize_b.load(Ordering::Relaxed) {
+            self.b.pop().or_else(|| self.a.pop())
+        } else {
+            self.a.pop().or_else(|| self.b.pop())
+        }
+    }
+
+    fn pop_n(&self, n: u32) -> PopN<'_, T> {
+        if self.prioritize_b.load(Ordering::Relaxed) {
+            let a = self.b.pop_n(n);
+            let rem = n - a.len() as u32;
+            let b = self.a.pop_n(rem);
+            PopN { a, b }
+        } else {
+            let a = self.a.pop_n(n);
+            let rem = n - a.len() as u32;
+            let b = self.b.pop_n(rem);
+            PopN { a, b }
+        }
+    }
+
+    /// # Safety
+    /// - You must be the exclusive publisher
+    unsafe fn try_publish(&self, data: &mut Vec<T>) {
+        let a = self.a.tail.load(Ordering::Relaxed) == 0;
+        let b = self.b.tail.load(Ordering::Relaxed) == 0;
+        if !a && !b {
+            return;
+        }
+
+        let l = data.len() == 0;
+        if a && b && l {
+            return;
+        }
+
+        let prioritize_b = self.prioritize_b.load(Ordering::Relaxed);
+        let buffers = if prioritize_b {
+            [(&self.b, b), (&self.a, a)]
+        } else {
+            [(&self.a, a), (&self.b, b)]
+        };
+        if buffers[0].1 {
+            self.prioritize_b.fetch_not(Ordering::Relaxed);
+        }
+        for (buffer, is_empty) in buffers {
+            if is_empty {
+                continue;
+            }
+            // `Acquire` any writes. We don't need the actual value because we already checked that the value
+            // was zero and once the `tail` is 0 the only possible writer is the single producer (aka this function)
+            let _ = buffer.tail.load(Ordering::Acquire);
+            // Safety: Caller ensures they are the only publisher.
+            // We checked if the prioritized buffer is empty.
+            unsafe {
+                buffer.publish(data);
+            }
+        }
+    }
+}
+
+/// Manual Chain operation. Theoretically more optimizable because Chain uses Option<T> for portability.
+pub struct PopN<'a, T> {
+    a: InnerPopN<'a, T>,
+    b: InnerPopN<'a, T>,
+}
+
+impl<'a, T> Iterator for PopN<'a, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.a.next().or_else(|| self.b.next())
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.a.size_hint().0 + self.b.size_hint().0;
+        (len, Some(len))
+    }
+}
+
+impl<'a, T> ExactSizeIterator for PopN<'a, T> {}
+impl<'a, T> FusedIterator for PopN<'a, T> {}
 
 /// A reader for a [`FreeList`] that provides methods for popping values.
 #[derive(Debug, Clone)]
